8e4e937bb15f174e3da6035c5b7c9bb0ac57f69c
Yanik2 <wm_yanik@ukr.net>
2025-10-01 16:59:57 +0200

    add logic

--- src/main/java/com/example/SourceFunction.java
+++ src/main/java/com/example/SourceFunction.java
@@ -1,14 +1,25 @@
 package com.example;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.cloud.functions.CloudEventsFunction;
+import com.google.events.cloud.pubsub.v1.MessagePublishedData;
 import io.cloudevents.CloudEvent;
+import java.util.Base64;
 import java.util.logging.Logger;
 
 public class SourceFunction implements CloudEventsFunction {
     private static final Logger logger = Logger.getLogger(SourceFunction.class.getName());
+    private static final ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
     public void accept(CloudEvent cloudEvent) throws Exception {
-        logger.info("This is message from source cloud function");
+        final var message = objectMapper.readValue(
+            new String(cloudEvent.getData().toBytes()),
+            MessagePublishedData.class
+        );
+
+        final var decodedMessage = Base64.getDecoder().decode(message.getMessage().getData().toByteArray());
+
+        logger.info("This is message from source cloud function: " + new String(decodedMessage));
     }
 }
